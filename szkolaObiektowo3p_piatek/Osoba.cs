2558bcc1788efa8250c0441160c9dcec42e69a39
markup54 <marzenakupka@tlen.pl>
2026-09-18 13:37:07 +0200

    klasa Osoba konstruktor

---
+++ szkolaObiektowo3p_piatek/Osoba.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szkolaObiektowo3p_piatek
+{
+    public class Osoba
+    {
+        public string imie;
+        public string nazwisko;
+        public int wiek;
+
+        public Osoba()
+        {
+        }
+
+        public Osoba(string imie, string nazwisko, int wiek)
+        {
+            //this.imie - pole klasy
+            //imie - zmienna lokalna w tej metodzie
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.wiek = wiek;
+        }
+
+        public override string? ToString()
+        {
+            return "imię: "+imie+"\n nazwisko: "+nazwisko;
+        }
+    }
+}
--- szkolaObiektowo3p_piatek/Program.cs
+++ szkolaObiektowo3p_piatek/Program.cs
@@ -1,5 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using szkolaObiektowo3p_piatek;
+
 Console.WriteLine("Hello, World!");
 
 
+//klasa Osoba 
+//w JAvie każda klasa w oddzielnym pliku tu może być w jednym
+//testowanie klasy osoba
+//modyfikatory dostępu
+//pola metody konstruktor
+
+Osoba osoba = new Osoba();
+//nowy obiekt klasy Osoba
+osoba.imie = "Jaś";
+osoba.nazwisko = "Muchomorek";
+osoba.wiek = 12;
+Console.WriteLine("imię: "+osoba.imie);
+Console.WriteLine(osoba);
+
+Osoba osoba2 = new Osoba("Ala","Jarzębinka",10);
+Console.WriteLine(osoba2);
+Console.WriteLine(osoba2.imie);
 

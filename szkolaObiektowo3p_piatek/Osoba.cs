fa8e07be2d249eb96384dbdaf558b5383b70e59f
markup54 <marzenakupka@tlen.pl>
2026-09-18 13:59:09 +0200

    modyfikatory dostępu

--- szkolaObiektowo3p_piatek/Osoba.cs
+++ szkolaObiektowo3p_piatek/Osoba.cs
@@ -11,11 +11,22 @@ namespace szkolaObiektowo3p_piatek
         private string imie;
         private string nazwisko;
         private int wiek;
+        /*
+         * modyfikatory dostępu
+         * private - pola dostępne tylko w tej klasie
+         * protected - pola dostępne w tej klasie i klasach dziedziczących
+         * public - ogólnodostępne
+         * stosowanie hermetyzacji wymaga pól i metod prywatnych lub chronionych
+         * hermetyzacja  == enkapsulacja
+         * żeby się dostąc do prywatnych pól należy stosować metody dostępowe
+         * 
+         */
 
         public Osoba()
         {
         }
-
+        //dwa konstruktory/metody o takich samych nazwach ale różnych
+        //parametrach to przeciążanie
         public Osoba(string imie, string nazwisko, int wiek)
         {
             //this.imie - pole klasy
--- szkolaObiektowo3p_piatek/Program.cs
+++ szkolaObiektowo3p_piatek/Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine("Hello, World!");
 //modyfikatory dostępu
 //pola metody konstruktor
 
-Osoba osoba = new Osoba();
+Osoba osoba = new Osoba();//wywołanie konstruktora
 //nowy obiekt klasy Osoba
 /*
  * niedostępne dla pól prywatnych 
@@ -21,7 +21,7 @@ osoba.wiek = 12;
 Console.WriteLine("imię: "+osoba.imie);*/
 Console.WriteLine(osoba);
 
-Osoba osoba2 = new Osoba("Ala","Jarzębinka",10);
+Osoba osoba2 = new Osoba("Ala","Jarzębinka",10);//wywołanie konstruktora
 Console.WriteLine(osoba2);
 //Console.WriteLine(osoba2.imie);
 
